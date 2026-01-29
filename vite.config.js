bbef3c0a7bcd5e6ff4f41e7e77d5d5d8759e6dad
Andrew Ngumbao <dremukare@gmail.com>
2026-01-29 17:44:00 +0300

    chore: configuring proxy in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'https://zenquotes.io/api/',
+				changeOrigin: true,
+				secure: false,
+				rewrite: (path) => path.replace(/^\/api/, ''),
+			},
+			cors: false,
+		},
+	},
+});
