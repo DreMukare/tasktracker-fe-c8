c15c0e4ccb4e2240795754f4f9701dfca2223b65
Andrew Ngumbao <dremukare@gmail.com>
2026-02-19 17:17:32 +0300

    feat: handle pending and error states for quote

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,27 +3,32 @@ import TaskItem from './TaskItem';
 import useTaskStore from '../stores/task';
 import useQuoteStore from '../stores/quote';
 import useQuotes from '../hooks/quotes';
-import { useEffect } from 'react';
 import useTheme from '../stores/theme';
 
 const TaskList = () => {
 	const { tasks, deleteTask } = useTaskStore();
 	const { h1Styling, h4Styling, pStyling } = useTheme();
 
-	useEffect(() => {
-		useQuotes();
-	}, []);
+	const { isPending, error, data } = useQuotes();
 
 	const { quoteOfTheDay } = useQuoteStore();
 
 	return (
 		<div>
 			<h2 className={`${h1Styling} text-center my-[2em]`}>Task List</h2>
-			{quoteOfTheDay[0] && (
+			{data && quoteOfTheDay[0] && (
 				<div className="mx-auto text-center mb-[2em]">
 					<p className={`${pStyling} text-xl`}>{quoteOfTheDay[0]['q']}</p>
 					<h4 className={h4Styling}>~ {quoteOfTheDay[0]['a']}</h4>
 				</div>
+			)}
+			{isPending && (
+				<p className={`${pStyling} text-center mb-2`}>Fetching your quote...</p>
+			)}
+			{error && (
+				<p className={`${pStyling} text-red-500 text-center mb-2`}>
+					Something went wrong fetching the quote
+				</p>
 			)}
 			<AddTaskForm />
 			<ul className="mx-auto my-[2em] max-w-[60vw]">
